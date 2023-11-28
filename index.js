3d504e3d2a5df3cdcded3b66a24a1c7af5240631
lokesh shriwas <lokeshshriwas2802@gmail.com>
2023-11-29 00:43:06 +0530

    wrapped everything in try catch block

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(express.json())
 const mongoose = require("mongoose");
 const { stringify } = require("nodemon/lib/utils");
 const { rmSync } = require("fs");
+const { nextTick } = require("process");
+const { errorMonitor } = require("events");
 
 main()
 .then(res=> console.log("connection stablised"))
@@ -50,57 +52,89 @@ app.get('/', function (req, res) {
 
  
 // post home route (create)
-app.post("/home", function(req, res){
+app.post("/home", async (req, res, next)=>{
+   try {
     const {name: username, education: edu,  age: ag} = req.body
-    User.create({
+    await User.create({
         name: username,
         age: ag,
         education: edu
     })
     res.redirect("/read")
+   } catch (error) {
+        next(error)
+   }
 })
 
 // read route (read)
 
-app.get("/read", async (req, res)=>{
+app.get("/read", async (req, res,next)=>{
+  try {
     let mainRes = await User.find()
+    if(!mainRes){
+        throw new expressError(404,"page not found")
+    }
     res.render("read", {mainRes})
-   })
+  } catch (error) {
+    next(error)
+  }
+})
 
 
 // edit route
 
-app.get("/read/:id/edit", async (req, res)=>{
-    const {id: trgId} = req.params
-    const trgRes =  await User.findOne({_id: `${trgId}`})
-    res.render("edit", {trgRes})
-    })
+app.get("/read/:id/edit", async (req, res, next)=>{
+    try {
+        const {id: trgId} = req.params
+        const trgRes =  await User.findOne({_id: `${trgId}`})
+        
+        if(!trgRes){
+            next( new expressError(402,"forbidden error found"))
+         }
+        res.render("edit", {trgRes})
+    } catch (error) {
+        next(error)
+    }
+})
 
 // edit patch route
 
-app.patch("/read/:id",async (req, res)=>{
-   const {id: trgId} = req.params
-   const {education: edu} = req.body
-     const trgRes = await User.findOne({_id: `${trgId}`})
-        if(trgRes.education != edu){
-           await User.updateOne({_id: trgRes.id}, {education: edu})
-            res.redirect("/read")
-        } else{
-            res.redirect("/read")
-        }
+app.patch("/read/:id",async (req, res, next)=>{
+    try {
+        const {id: trgId} = req.params
+        const {education: edu} = req.body
+          const trgRes = await User.findOne({_id: `${trgId}`})
+             if(trgRes.education != edu){
+                await User.updateOne({_id: trgRes.id}, {education: edu})
+                 res.redirect("/read")
+             } else{
+                 res.redirect("/read")
+             }
+    } catch (error) {
+        next(error)
+    }
     }
 )
 
 
-
 // delete route
 
-app.delete("/read/:id", async (req, res)=>{
-    const {id: delId} = req.params
-    await User.findByIdAndDelete(`${delId}`)
-    res.redirect("/read")
+app.delete("/read/:id", async (req, res, next)=>{
+    try {
+        const {id: delId} = req.params
+        await User.findByIdAndDelete(`${delId}`)
+        res.redirect("/read")
+    } catch (error) {
+        next(error)
+    }
 })  
 
+// error handling middlewares
+
+app.use((err, req, res, next) => {
+    let {status = 500 , message = "some error occured"} = err
+    res.status(status).send(message)
+})
 
 
 
